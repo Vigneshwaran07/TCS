b1eff03529023f03ec59b872d8e2edfa774008f4
Vigneshwaran V <43519088+Vigneshwaran07@users.noreply.github.com>
2020-07-15 18:24:55 +0530

    Create Min_Combination(gcd).py

---
+++ Daily_practice/Min_Combination(gcd).py
@@ -0,0 +1,6 @@
+import math
+for _ in range(int(input())):
+    ans = math.gcd(int(input()), int(input()))
+    print(ans)
+#Further reference
+#https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
