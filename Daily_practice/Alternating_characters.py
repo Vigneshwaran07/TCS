6641ffe7f4309cda024e4b738a681db9fc5040ce
Vigneshwaran V <43519088+Vigneshwaran07@users.noreply.github.com>
2020-07-17 14:42:38 +0530

    Create Alternating_characters.py

---
+++ Daily_practice/Alternating_characters.py
@@ -0,0 +1,20 @@
+from itertools import groupby
+from itertools import combinations
+
+n = int(input())
+s = input()
+unique = set(s)
+delCount = len(unique) - 2
+ans = 0
+if(len(unique) > 1):
+    for i in combinations(unique, delCount):
+        temp = s
+        count = 0
+        for j in i:
+            temp = temp.replace(j, '')
+        if(all(temp[k]!=temp[k+1] for k in range(len(temp)-1))):
+            if(ans < len(temp)):
+                ans = len(temp)
+    print(ans)
+else:
+    print(ans)
